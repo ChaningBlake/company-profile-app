f0ca8dfa3b737a46883013641d78e9c569907328
Blake Mullinax <blakemullinax@charter.net>
2019-07-26 09:29:51 +0800

    created db file

---
+++ db_creator.py
@@ -0,0 +1,25 @@
+# db_creator.py
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+ 
+engine = create_engine('sqlite:///profiledata.db', echo=True)
+Base = declarative_base()
+ 
+class Company(Base):
+	__tablename__ = "companies"
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	upnew = Column(Float)
+
+	def __init__(self):
+		pass
+	
+	def __repr__(self):
+		return "<Company: {}>".format(self.name)
+ 
+# create tables
+Base.metadata.create_all(engine)
