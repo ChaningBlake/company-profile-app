55febb797d75e9a6ca49bf162694041f85e0921b
Blake Mullinax <blakemullinax@charter.net>
2019-07-26 16:58:27 +0800

    fixed database

--- db_creator.py
+++ db_creator.py
@@ -1,7 +1,7 @@
 # db_creator.py
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
  
@@ -13,11 +13,35 @@ class Company(Base):
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
-	upnew = Column(Integer)
+	upnew = Column(Float)
 	offerprice = Column(Integer)
+	auditor4 = Column(Integer)
+	internet = Column(Integer)
+	revisionnew = Column(Float)
+	lnage1 = Column(Float)
+	lnproceeds = Column(Float)
+	vc = Column(Integer)
+	lnsales = Column(Float)
+	leverage = Column(Float)
+	hot05 = Column(Integer)
+	lntanew = Column(Float)
 
-	def __init__(self):
-		pass
+
+	def __init__(self, name, upnew, offerprice, auditor4,
+	internet, revisionnew, lnage1, lnproceeds, vc, lnsales, leverage, hot05, lntanew):
+		self.name = name
+		self.upnew = upnew
+		self.offerprice = offerprice
+		self.auditor4 = auditor4
+		self.internet = internet
+		self.revisionnew = revisionnew
+		self.lnage1 = lnage1
+		self.lnproceeds = lnproceeds
+		self.vc = vc
+		self.lnsales = lnsales
+		self.leverage = leverage
+		self.hot05 = hot05
+		self.lntanew = lnta
 	
 	def __repr__(self):
 		return "<Company: {}>".format(self.name)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from db_setup import init_db
 
 # set up flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiledata.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "deloitte competition"
 
 # set up database
 db = SQLAlchemy(app)
-
-from app import routes, models
-
+init_db()
 
 @app.route("/")
 def home():
