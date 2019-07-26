f1f68bac29f4b6cbcfdbdc7b08d7911f1077e974
Blake Mullinax <blakemullinax@charter.net>
2019-07-26 09:21:46 +0800

    updated configuration

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# set up flask app
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# set up database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
+
 
 @app.route("/")
 def home():
