bd709dd6cfd49d0808639021f6fa4876e9339be5
Blake Mullinax <blakemullinax@charter.net>
2019-10-03 10:53:15 -0500

    Updated layout

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
+'''
+@author: Blake Mullinax
+Code created July 2019
+'''
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from db_setup import init_db
 
 # set up flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiledata.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "deloitte competition"
-
-# set up database
-db = SQLAlchemy(app)
-init_db()
 
 @app.route("/")
 def home():
@@ -22,15 +17,7 @@ def get_result():
 	y = int(request.form['paramB'])
 	z = int(request.form['paramC'])
 	calc1 = round((.32*x + .45*y + .23*z), 2)
-	calc2 = round((.50*x + .14*y + .36*z), 2)
-	calc3 = round((.25*x + .59*y + .16*z), 2)
-	calc4 = round((.60*x + .25*y + .15*z), 2)
 	
-	return render_template("result.html",
-	                        result=calc1,
-							result2 = calc2,
-							result3 = calc3,
-							result4 = calc4)
-
+	return render_template("result.html", result=calc1)
 if __name__ == "__main__":
     app.run(debug=True)
